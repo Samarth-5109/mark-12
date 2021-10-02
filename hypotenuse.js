b2565b5e123ac12629b7a250591814f8a98a9356
Samarth Bhardwaj <samarthb5109@gmail.com>
2021-10-02 16:25:58 +0530

    debugged

--- hypotenuse.js
+++ hypotenuse.js
@@ -7,9 +7,14 @@ function calculateHypotenuse(){
     var lengthOfHypotenuse;
     var sideOne = Number(sideA.value);
     var sideTwo = Number(sideB.value);
-
-    lengthOfHypotenuse = Math.sqrt((sideOne*sideOne) + (sideTwo*sideTwo));
+    if(sideOne=="" || sideTwo=="" || sideOne<0 || sideTwo<0){
+        hypoOutput.innerText = "Sides should not be empty, negative and 0 ";
+    }else{
+        lengthOfHypotenuse = Math.sqrt((sideOne*sideOne) + (sideTwo*sideTwo));
     hypoOutput.innerText = "The Length of hypotenuse is " + lengthOfHypotenuse;
+    }
+
+    
 }
 
 calculateButton.addEventListener("click", calculateHypotenuse);
